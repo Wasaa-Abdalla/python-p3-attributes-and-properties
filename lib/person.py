a26de4f6081b954c08f916b49262087a40645b71
abdalla-wasaa <abdallawasaa7@gmail.com>
2024-01-27 10:26:13 +0300

    Attempted

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 
 APPROVED_JOBS = [
     "Admin",
@@ -16,4 +15,34 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self):
+        self._name = ""
+        self._job = ""
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) >= 25:
+            print("Name must be a string under 25 characters.")
+        else:
+            self._name = value.title()  # Convert name to title case
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in APPROVED_JOBS:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self._job = value
+
+
+
+
+
+
